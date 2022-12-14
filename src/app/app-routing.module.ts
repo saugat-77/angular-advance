af4f5a8233423403e3099f87fdf307f9abf0bfb1
saugat77 <saugatacharya789@gmail.com>
2022-12-14 09:58:01 +0545

    todo UI setup

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {TodoComponent} from './forms/todo/todo.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'todo', pathMatch: 'full'},
+  {path: 'todo', component: TodoComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TodoComponent } from './forms/todo/todo.component';
+import { TodoModule } from './forms/todo/todo.module';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot({}, {}),
+    BrowserAnimationsModule,
+    TodoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
